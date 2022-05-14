1ca86abd68367f6af49fc6de1f3449f82c19052d
unknown <Luke@DESKTOP-GOUJAPF>
2022-05-14 18:52:58 +1000

    Simple web scraping

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Input";
+            this.textBox1.Size = new System.Drawing.Size(298, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(397, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(391, 409);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private RichTextBox richTextBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,37 @@
 namespace LeagueDrafter
 {
+    using HtmlAgilityPack;
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var htmk = await GetHtml();
+            ParseHtmlUsingHtmlAgilityPack(htmk);
+            System.Diagnostics.Debug.WriteLine("click");
+        }
+
+        private static Task<string> GetHtml()
+        {
+            var client = new HttpClient();
+            return client.GetStringAsync("https://github.com/XnipS");
+        }
+
+        private static void ParseHtmlUsingHtmlAgilityPack(string html)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var name = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='js-pinned-items-reorder-container']/ol/li/div/div").InnerText;
+            System.Diagnostics.Debug.WriteLine(name);
+        }
+
     }
 }
